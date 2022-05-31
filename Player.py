917b091d650a79c81289960b51a3ea3e0da03123
Megan Phillips <megphillips21@gmail.com>
2022-05-30 17:55:57 -0700

    added player parent class

---
+++ Player.py
@@ -0,0 +1,10 @@
+# Parent class- similarities for both human and ai-
+
+class Player:
+    def __init__(self):
+        self.name = ""
+        self.skills_list = ["Rock","Paper","Scissors","Lizard", "Spock"]
+        pass
+
+    def choose_skill(self):
+        pass
